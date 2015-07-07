69359ee508bb1eacb656bd37faf5b3b80b04d371
dhxie <xie5721072@163.com>
2015-07-07 12:45:52 +0800

    add

--- h51/1.js
+++ h51/1.js
@@ -36,7 +36,7 @@ $(function () {
     });
 
     play.addEventListener("canplaythrough", function () {
-
+            runPage.go(1);
         }
     );
 
@@ -73,7 +73,7 @@ $(function () {
     var counter = 0;
     for (var i = 0, img; i < len; i++) {
         img = $("img")[ i ];
-        if(!img.getAttribute("load")){
+        if (!img.getAttribute("load")) {
             img.src = img.getAttribute("url");
         }
         img.onload = function () {
@@ -83,19 +83,19 @@ $(function () {
         };
     }
 
-    window.onorientationchange = function () {
-        console.log(window.orientation);
-        switch (window.orientation) {
-            case -90:
-            case 90:
-
-                break;
-            case 0:
-            case 180:
-                //alert("竖屏:" + window.orientation);
-                break;
-        }
-    };
+    //window.onorientationchange = function () {
+    //    console.log(window.orientation);
+    //    switch (window.orientation) {
+    //        case -90:
+    //        case 90:
+    //
+    //            break;
+    //        case 0:
+    //        case 180:
+    //            //alert("竖屏:" + window.orientation);
+    //            break;
+    //    }
+    //};
 
     function load() {
         $("#musiccontrolon").addClass("ele_hide");
@@ -117,7 +117,7 @@ $(function () {
         mode: 'wheel,touch',               // mode of fullpage
         easing: 'ease',                                // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1] )
         callback: function (index, thisPage) {
-            switch (index){
+            switch (index) {
                 case 0:
 
                     break;
@@ -125,7 +125,7 @@ $(function () {
                     play_page1();
                     play.play();
                     $("#musiccontrolon").removeClass("ele_hide");
-                    runPage.removePage(0);
+                    runPage.removePage('page1');
                     break;
             }
         }
@@ -134,12 +134,12 @@ $(function () {
     load();
     load_before();
 
-    window.onload = window.onresize = function(){
+    window.onload = window.onresize = function () {
         var page = new pageResponse({
-            class : 'contain',     //模块的类名，使用class来控制页面上的模块(1个或多个)
-            mode : 'contain',     // auto || contain || cover
-            width : '320',      //输入页面的宽度，只支持输入数值，默认宽度为320px
-            height : '568'      //输入页面的高度，只支持输入数值，默认高度为504px
+            class: 'contain',     //模块的类名，使用class来控制页面上的模块(1个或多个)
+            mode: 'contain',     // auto || contain || cover
+            width: '320',      //输入页面的宽度，只支持输入数值，默认宽度为320px
+            height: '568'      //输入页面的高度，只支持输入数值，默认高度为504px
         })
     }
 });
