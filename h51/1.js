5b812565793d7ba552d1ed17ccf744964701184b
dhxie <xie5721072@163.com>
2015-07-13 14:14:21 +0800

    add

--- h51/1.js
+++ h51/1.js
@@ -1,63 +1,68 @@
 $(function () {
-    var play = document.getElementById('fMusicPlayer');
+    var runPage//框架
+        , init_auto_start
+        , play = document.getElementById('fMusicPlayer');
+
+    var base_rate = {
+        defaultWidth: 320,
+        defaultHeight: 568,
+        newWidth: document.documentElement.clientWidth,
+        newHeight: document.documentElement.clientHeight
+    };
 
     $('html').one('touchstart', function () {
-        play.play();
+        //play.play();
+        if (!init_auto_start) {
+            runPage.go(1);
+        }
     });
 
     ////禁止窗口的默认滑动
     document.ontouchmove = function (e) {
         e.preventDefault();
     };
-    //框架
-    var runPage,
-        interval,
-        autoPlay;
 
-    autoPlay = function (to) {
-
-        clearTimeout(interval);
-        interval = setTimeout(function () {
-            runPage.go(to);
-        }, 5000);
-
-    };
-
-    $('#musiccontrolon').click(function () {
+    //点击
+    $('#musiccontrolon').tap(function () {
         play.pause();
         $("#musiccontrolon").addClass("ele_hide");
         $("#musiccontroloff").removeClass("ele_hide");
     });
 
-    $('#musiccontroloff').click(function () {
+    $('#musiccontroloff').tap(function () {
         play.play();
         $("#musiccontroloff").addClass("ele_hide");
         $("#musiccontrolon").removeClass("ele_hide");
     });
 
     play.addEventListener("canplaythrough", function () {
-            runPage.go(1);
+            if (!init_auto_start) {
+                runPage.go(1);
+                $("#musiccontrolon").addClass("auto_at");
+            }
         }
     );
 
-    function play_page1() {
-        change_status(".moveUp", "running");
-        change_status(".arrowing", "running");
-        change_status(".fadeInNormal", "running");
-        change_status(".expandOpen", "running");
-        change_status(".fadeIn", "running");
-    }
-
-    function stop_page1() {
-        change_status(".moveUp", "paused");
-        change_status(".arrowing", "paused");
-        change_status(".fadeInNormal", "paused");
-        change_status(".expandOpen", "paused");
-        change_status(".fadeIn", "paused");
-    }
-
+    //初始前加载
     function load_before() {
+        load();
         auto_bt();
+    }
+
+    //修复缩放比率
+    function reset_size(obj) {
+        var ele = obj.ele
+            , wProp = parseFloat(obj.newWidth / obj.defaultWidth).toFixed(2)
+            , hProp = parseFloat(obj.newHeight / obj.defaultHeight).toFixed(2)
+            , eleWidth = parseInt(ele.style.width) * wProp
+            , eleHeight = parseInt(ele.style.height) * hProp
+            , eleLeft = (ele.style.left.indexOf("%") === -1) ? parseInt(ele.style.left) * wProp : null
+            , eleTop = parseInt(ele.style.top) * hProp;
+
+        ele.style.top = eleTop + "px";
+        if (eleLeft)ele.style.left = eleLeft + "px";
+        ele.style.width = eleWidth + "px";
+        ele.style.height = eleHeight + "px";
     }
 
     function auto_bt() {
@@ -78,24 +83,20 @@ $(function () {
         }
         img.onload = function () {
             counter++;
-            if (counter >= len) {
+            if (counter >= len - 1) {
+                if (!init_auto_start) {
+                    runPage.go(1);
+                    $("#musiccontrolon").addClass("auto_at");
+                }
             }
         };
     }
 
-    //window.onorientationchange = function () {
-    //    console.log(window.orientation);
-    //    switch (window.orientation) {
-    //        case -90:
-    //        case 90:
-    //
-    //            break;
-    //        case 0:
-    //        case 180:
-    //            //alert("竖屏:" + window.orientation);
-    //            break;
-    //    }
-    //};
+    function img_lazy_load() {
+        $('div[resize="true"]').each(function (index, ele) {
+            reset_size($.extend(base_rate, {ele: ele}));
+        });
+    }
 
     function load() {
         $("#musiccontrolon").addClass("ele_hide");
@@ -105,25 +106,24 @@ $(function () {
     runPage = new FullPage({
         id: 'pageContain',                            // id of contain
         slideTime: 800,                               // time of slide
-        continuous: false,                             // create an infinite feel with no endpoints
+        continuous: false,                            // create an infinite feel with no endpoints
         effect: {                                     // slide effect
             transform: {
-                translate: 'Y',					   // 'X'|'Y'|'XY'|'none'
-                scale: [ 1, 1 ],					   // [scalefrom, scaleto]
-                rotate: [ 0, 0 ]					   // [rotatefrom, rotateto]
+                translate: 'Y',					      // 'X'|'Y'|'XY'|'none'
+                scale: [ 1, 1 ],					  // [scalefrom, scaleto]
+                rotate: [ 0, 0 ]					  // [rotatefrom, rotateto]
             },
-            opacity: [ 0, 1 ]                           // [opacityfrom, opacityto]
+            opacity: [ 0, 1 ]                         // [opacityfrom, opacityto]
         },
-        mode: 'wheel,touch',               // mode of fullpage
-        easing: 'ease',                                // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1] )
+        mode: 'wheel,touch',                          // mode of fullpage
+        easing: 'ease',                               // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1] )
         callback: function (index, thisPage) {
             switch (index) {
                 case 0:
 
                     break;
                 case 1:
-                    play_page1();
-                    play.play();
+                    //play.play();
                     $("#musiccontrolon").removeClass("ele_hide");
                     runPage.removePage('page1');
                     break;
@@ -131,15 +131,20 @@ $(function () {
         }
     });
 
-    load();
-    load_before();
 
     window.onload = window.onresize = function () {
-        var page = new pageResponse({
-            class: 'contain',     //模块的类名，使用class来控制页面上的模块(1个或多个)
-            mode: 'contain',     // auto || contain || cover
-            width: '320',      //输入页面的宽度，只支持输入数值，默认宽度为320px
-            height: '568'      //输入页面的高度，只支持输入数值，默认高度为504px
-        })
-    }
+        var body_width = document.documentElement.clientWidth
+            , body_height = document.documentElement.clientHeight
+            , page = new pageResponse({
+                class: 'contain',     //模块的类名，使用class来控制页面上的模块(1个或多个)
+                mode: 'contain',      // auto || contain || cover
+                width: (body_width > 414) ? 320 : body_width,      //输入页面的宽度，只支持输入数值，默认宽度为320px
+                height: (body_height > 736) ? 504 : body_height    //输入页面的高度，只支持输入数值，默认高度为504px
+            });
+        if (body_width <= 414 && body_height <= 736) {
+            img_lazy_load();
+        }
+    };
+
+    load_before();
 });
